9b2d49684ad751f4899bea57b4b3adfba4b0c82d
resul_dagdanov <resul.dagdanov@eatron.com>
2022-01-23 19:06:59 +0300

    config params are redefined for imitation training

--- agents/imitation_training/config.py
+++ agents/imitation_training/config.py
@@ -13,31 +13,25 @@ time_info = current_date + "-" + current_time
 base_path = "/home/resul/Research/Codes/Carla/scenario-based-rl" # Local
 # base_path = "/cta/eatron/CarlaChallenge/ea202101001_platooning_demo/carla_ws" # WorkStation
 
+# dataset for imitation learning (training)
+training_data_path = base_path + "/checkpoint/dataset/dataset_4_workstation/" # Local
+# training_data_path = "/cta/eatron/CarlaChallenge/TransFuser/data/14_weathers_data/" # WorkStation
+
+# dataset for imitation learning (validation)
+validation_data_path = base_path + "/checkpoint/dataset/dataset_4_workstation/" # Local
+# validation_data_path = "/cta/eatron/CarlaChallenge/TransFuser/data/14_weathers_data/" # WorkStation
+
 best_model_date = "Nov_07_2021-15_32_09"
 best_model_name = "epoch_290.pth"
 
 # whether to train a pre-trained model
 pretrained = False
 
-# whether to use aggregated data (DAgger)
-use_dagger_data = False
-
-# network output is whether brake or throttle-steer. note that to use 'action_classifier', some comments has to be uncommented
-type_of_training = "brake_classifier"
-
 # location of all trained and saved models
 model_save_path = base_path + "/checkpoint/results/" + time_info + "/"
 
 # latest trained model
-trained_model_path = base_path + "/checkpoint/" + best_model_date + "/" + type_of_training + "/" + best_model_name
-
-# dataset for imitation learning (training)
-training_data_path = base_path + "/checkpoint/dataset/test/" # Local
-# training_data_path = "/cta/eatron/CarlaChallenge/TransFuser/data/14_weathers_data/" # WorkStation
-
-# dataset for imitation learning (validation). will be used also for training dagger data
-validation_data_path = base_path + "/checkpoint/dataset/test/" # Local
-# validation_data_path = "/cta/eatron/CarlaChallenge/TransFuser/data/14_weathers_data/" # WorkStation
+trained_model_path = base_path + "/checkpoint/" + best_model_date + "/" + best_model_name
 
 # aggregated data path. will be concatenated with training dataset
 dagger_data_path = base_path + "/dataset/dagger/"
@@ -49,26 +43,18 @@ momentum = 0.9
 gamma = 0.1
 min_milestone = 10
 max_milestone = 20
-max_number_of_epochs = 300
+max_number_of_epochs = 50
 loss_print_interval = 32
 speed_input_size = 120
 
 # model saving frequency
-save_every_n_epoch = 10
+save_every_n_epoch = 1
 
-# test on validation set every 10 epoch
-validate_per_n = 10
+# test on validation set every epoch
+validate_per_n = 1
 
-train_towns = ["town05_tiny"] # Local
-# train_towns = [ # WorkStation
-#         "Town01_long", "Town01_short", "Town01_tiny", \
-#         "Town02_long", "Town02_short", "Town02_tiny", \
-#         "Town03_long", "Town03_short", "Town03_tiny", \
-#         "Town04_long", "Town04_short", "Town04_tiny", \
-#                         "Town05_short", "Town05_tiny", \
-#         "Town06_long", "Town06_short", "Town06_tiny", \
-#                         "Town07_short", "Town07_tiny",\
-#                         "Town10_short", "Town10_tiny"]
+train_towns = [ "town01_long", "town01_short", "town01_tiny",
+                "town05_long", "town05_short", "town05_tiny",
+                "town03_short"]
 
-validation_towns = ["town01_tiny"] # Local
-# validation_towns = ["Town05_long"] # WorkStation
+validation_towns = ["town03_long"]
