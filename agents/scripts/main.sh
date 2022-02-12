3097d7e2c459ecc390ac455393c0890f1f367017
resul_dagdanov <resul.dagdanov@eatron.com>
2022-02-12 11:33:59 +0300

    constant config and name changes

--- agents/scripts/main.sh
+++ agents/scripts/main.sh
@@ -6,13 +6,13 @@ SECONDS=0
 # global variables
 export imitation_learning=false # to evaluate imitation learning model make it true, to train or evaluate dqn model make it false
 export evaluate=false # true
-export model_name="Feb_01_2022-03_46_25" # only used if evaluate true, make sure it exists
-export load_episode_number=1050 # only used if evaluate true, make sure it exists
+export model_name="Feb_07_2022-14_26_30" # only used if evaluate true, make sure it exists
+export load_episode_number=300 # only used if evaluate true, make sure it exists
 
-export repetitions=1 #50
-export max_episode_batch_num=1 #20
+export repetitions=50 #50
+export max_episode_batch_num=50 #20
 export json_file="all_towns_traffic_scenarios_WOR.json" #"town05_all_scenarios.json" #
-export xml_file="failed_routes/town05_short/traffic_light_3.xml" #"failed_routes/town03_short/traffic_junction_1.xml" # #"failed_routes/town05_long/stuck_vehicle_1.xml"
+export xml_file="failed_routes/town05_long/stuck_vehicle_1.xml" #"failed_routes/town03_short/traffic_junction_1.xml"
 
 max_episode_num=`expr $repetitions \* $max_episode_batch_num`
 
